584a21bf70391d0e391f449b4b975424d7773ca7
RenanTelini <renan_telini@hotmail.com>
2022-03-21 21:27:26 -0300

    BD e modelagem alterada

--- HomeDelasDB.sql
+++
@@ -1,6 +0,0 @@
-create database HomeDelasDB;
-
-use HomeDelasDB;
-
-select cad.id, cad.nome_empresa, cad.nome_vaga, cad.descricao_vaga, cad.vaga_link, cad.linkedin_empresa
-from cadastro_vagas as cad;
